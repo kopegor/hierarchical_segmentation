d77c76c3b17bb2533e80197d0bcec0f8986ebb55
egor.koptelov <egor.koptelov@aramcoinnovations.com>
2024-08-26 08:43:18 +0000

    Run experiment v9 - updated pipeline

--- pose_estimation_sam_module.py
+++ pose_estimation_sam_module.py
@@ -22,7 +22,6 @@ class PascalPartModel(pl.LightningModule):
         super(PascalPartModel, self).__init__()
 
         # Loss functions
-        # self.bce_loss = nn.BCEWithLogitsLoss()
         self.cross_entropy_loss = nn.CrossEntropyLoss(
             # ignore_index=0
             )
@@ -38,12 +37,7 @@ class PascalPartModel(pl.LightningModule):
         self.transform = transform
 
         # Create a shared encoder using SMP (Segmentation Models PyTorch)
-        # self.model = smp.MAnet(
-        #     encoder_name=encoder_name,        
-        #     encoder_weights=encoder_weights,  
-        #     classes=2,                     
-        #     activation=None                   
-        # )
+        
         self.model = smp.DeepLabV3(  
             encoder_name=encoder_name,        
             encoder_weights=encoder_weights,  
@@ -55,10 +49,7 @@ class PascalPartModel(pl.LightningModule):
         self.freeze_encoder()
 
         # Fusion Module: Combine image features and mask features
-        # self.fusion_conv1d = nn.Conv2d(in_channels=4, out_channels=3, kernel_size=3, padding=1)
         self.body_conv1d = nn.Conv2d(in_channels=4, out_channels=3, kernel_size=3, padding=1)
-        # self.up_low_conv1d = nn.Conv2d(in_channels=6, out_channels=3, kernel_size=3, padding=1)
-        # self.parts_conv1d = nn.Conv2d(in_channels=9, out_channels=3, kernel_size=3, padding=1)
 
         self.decoder_body = self.create_decoder(self.model, num_classes=2)
         self.decoder_up_low = self.create_decoder(self.model, num_classes=3)
@@ -96,11 +87,8 @@ class PascalPartModel(pl.LightningModule):
         # run pose estimation model
         res_pose = model_pose.predict(
             x,
-            # iou=0.999,
             conf=0.001, 
-            # imgsz=640,
             imgsz=256,
-            # device='cpu'
             verbose=False
         )
 
@@ -111,17 +99,14 @@ class PascalPartModel(pl.LightningModule):
         bboxes = []
 
         for r in res_pose:
-
-            # tmp_points = r.keypoints.xy.reshape((-1, 2)).cpu().numpy().astype(np.uint8)
             tmp_points = r.keypoints.xy.reshape((-1, 2)).cpu().tolist()
             tmp_points += [[0, 0]]
             tmp_points = np.array(tmp_points, dtype=np.uint8)
             
             points.append(tmp_points)
 
-            labels.append(np.arange(len(tmp_points)).astype(np.uint8)) # .tolist())
+            labels.append(np.arange(len(tmp_points)).astype(np.uint8)) 
             
-            # tmp_bboxes = r.boxes.xywh.reshape((-1, 4)).cpu().numpy().astype(np.uint8)
             tmp_bboxes = r.boxes.xywh.reshape((-1, 4)).cpu().tolist()
             tmp_bboxes += [[0, 0, 0, 0]]
             tmp_bboxes = np.array(tmp_bboxes, dtype=np.uint8)
@@ -130,18 +115,14 @@ class PascalPartModel(pl.LightningModule):
         # Create a FastSAM model
         model_sam = FastSAM("FastSAM-s.pt")  # "FastSAM-x.pt"
 
-        # x = x.astype(np.uint8)
         res_seg = None
 
-        # for i in range(len(points)):
         try:
             res_seg = model_sam.predict(
                 source=x,
-                # device='gpu',
                 half=True,
                 verbose=False,
                 iou=0.99,
-                # imgsz=640,
                 imgsz=256,
                 points=points[0], 
                 labels=labels[0], 
@@ -151,49 +132,21 @@ class PascalPartModel(pl.LightningModule):
                 # texts= 'humans. for each human. upper body. lower body. low hand, up hand, torso, head, low leg, up leg'
             )
             
-            # res_seg.append(tmp_res)
-
+            
         except Exception as err:
-            # print('try without text promt')
-            # print(err)
-            # try:
-            #     res_seg = model_sam.predict(
-            #         source=x,
-            #         # device='gpu',
-            #         half=True,
-            #         verbose=False,
-            #         iou=0.99,
-            #         # imgsz=640,
-            #         imgsz=256,
-            #         points=points[0], 
-            #         labels=labels[0], 
-            #         bboxes=bboxes[0],
-            #         # conf=0.01,
-            #         # texts='human, body parts'
-            #         # texts= 'humans. for each human. upper body. lower body. low hand, up hand, torso, head, low leg, up leg'
-            #     )
-
-            #     # res_seg.append(tmp_res)
-            
-            # except Exception as err: 
-            
+          
             final_mask = torch.zeros(256, 256).to(device)
             print(err)
             print('added zeros feature map')
 
-                # print(f'zeros final mask shape: {final_mask.shape}')
-
             return final_mask
 
-
         # convert to final mask
-        
         final_mask = torch.sum(res_seg[0].masks.data.cpu(), dim=0).numpy()
         final_mask = np.array(final_mask)
         final_mask = torch.tensor(final_mask)
 
         final_mask = torch.tensor(final_mask).to(device)
-        # print(f'normal final mask shape: {final_mask.shape}')
 
         return final_mask
 
@@ -217,33 +170,29 @@ class PascalPartModel(pl.LightningModule):
         upper_lower_body_mask[(masks == 3) | (masks == 5)] = 2
         return upper_lower_body_mask
     
-    def _convert_parts_to_body(self, masks):
-        body_pred = torch.zeros_like(masks)
-        body_pred = body_pred[:, 0, :, :].unsqueeze(1)
-
-        for idx_logit, logit in enumerate(masks):
-            for idx_mask, mask in enumerate(logit):
-                body_pred[idx_logit, 0, mask >= 0.5] = 1 
-
-        return body_pred
-
-    def _convert_parts_to_up_low(self, masks):
-        up_low_pred = torch.zeros_like(masks)
-        up_low_pred = up_low_pred[:, 0, :, :].unsqueeze(1)
-
-        for idx_logit, logit in enumerate(masks):
-            for idx_mask, mask in enumerate(logit):
-                up_low_pred[idx_logit, 0, (mask == 1) | (mask == 2) | (mask == 4) | (mask == 6)] = 1 
-                # up_low_pred[idx_logit, 0, (0.5 < mask and mask< 2.5) or (3.5 < mask and mask < 4.5) or (5.5 < mask)] = 1 
-        
-        return up_low_pred
-
     def freeze_pipeline(self):
-        if self.current_epoch <= 1:
+        if self.current_epoch <= 2:
+            for param in self.decoder_body.parameters():
+                param.requires_grad = True
+
+            for param in self.decoder_up_low.parameters():
+                param.requires_grad = False
+
             for param in self.decoder_parts.parameters():
                 param.requires_grad = False
 
-        elif self.current_epoch <= 3:
+        elif self.current_epoch <= 5:
+            for param in self.decoder_body.parameters():
+                param.requires_grad = False
+
+            for param in self.decoder_up_low.parameters():
+                param.requires_grad = True
+
+            for param in self.decoder_parts.parameters():
+                param.requires_grad = False
+
+
+        elif self.current_epoch <= 10:
             for param in self.decoder_body.parameters():
                 param.requires_grad = False
 
@@ -253,93 +202,36 @@ class PascalPartModel(pl.LightningModule):
             for param in self.decoder_parts.parameters():
                 param.requires_grad = True
 
-
-        elif self.current_epoch <= 6:
-            for param in self.decoder_body.parameters():
-                param.requires_grad = True
-
-            for param in self.decoder_up_low.parameters():
-                param.requires_grad = True
-
-            for param in self.decoder_parts.parameters():
-                param.requires_grad = True
-
-        
         else:
+
             for param in self.decoder_body.parameters():
-                param.requires_grad = False
+                param.requires_grad = True
 
             for param in self.decoder_up_low.parameters():
-                param.requires_grad = False
+                param.requires_grad = True
 
             for param in self.decoder_parts.parameters():
                 param.requires_grad = True
-
 
 
     def forward(self, x):
         device = 'cuda' if x.get_device() >= 0 else 'cpu'
-        # extract features using pose estimation and SAM
-        # sam_features = self.get_features_mask(x)
-
-
-        # sam_features = [self.get_single_features_mask(x_idx).cpu().tolist() for x_idx in x]
-
-        # sam_features = torch.tensor(sam_features, dtype=torch.float32).to(device).unsqueeze(1)
-
-        # # concat image and extracted features from SAM     
-        # x_for_body = torch.cat([x, sam_features], dim=1)
-        # # x = x + sam_features
-
-        # x_for_body = self.body_conv1d(x_for_body)
-
-        # # Pass the input through the encoder for body segmentation
-        # output_body = self.model_body(x_for_body)
-        # # output_body = self.decoder_body(x_for_body)
-
-        # x_for_up_low = torch.cat([x, sam_features, output_body], dim=1)
-        # x_for_up_low = self.up_low_conv1d(x_for_up_low)
-
-        # # output_up_low = self.model_up_low(x_for_up_low)
-        # output_up_low = self.model_body.encoder(x_for_up_low)[-1]
-        # output_up_low = self.decoder_up_low(output_up_low)
-
-        # x_for_parts = torch.cat([x, sam_features, output_body, output_up_low], dim=1)
-        # x_for_parts = self.parts_conv1d(x_for_parts)
-
-        # # output_parts = self.model_parts(x_for_parts)
-        # output_parts = self.model_body.encoder(x_for_parts)[-1]
-        # output_parts = self.decoder_parts(output_parts)
-
+        
         sam_features = [self.get_single_features_mask(x_idx).cpu().tolist() for x_idx in x]
 
         sam_features = torch.tensor(sam_features, dtype=torch.float32).to(device).unsqueeze(1)
 
         # concat image and extracted features from SAM     
         x_for_body = torch.cat([x, sam_features], dim=1)
-        # x = x + sam_features
-
+       
         x_for_body = self.body_conv1d(x_for_body)
 
         # Pass the input through the encoder for body segmentation
         output_features_body = self.model.encoder(x_for_body)[-1]
         output_body = self.decoder_body(output_features_body)
-        # output_body = self.decoder_body(x_for_body)
-
-        # x_for_up_low = torch.cat([x, sam_features, output_body], dim=1)
-        # x_for_up_low = self.up_low_conv1d(x_for_up_low)
-
-        # output_up_low = self.model_up_low(x_for_up_low)
-        # output_up_low = self.model.encoder(x_for_up_low)[-1]
-        # output_up_low = self.decoder_up_low(output_up_low)
+        
         output_up_low = self.decoder_up_low(output_features_body)
 
-        # x_for_parts = torch.cat([x, sam_features, output_body, output_up_low], dim=1)
-        # x_for_parts = self.parts_conv1d(x_for_parts)
-
-        # output_parts = self.model_parts(x_for_parts)
-        # output_parts = self.model.encoder(x_for_parts)[-1]
-        # output_parts = self.decoder_parts(output_parts)
         output_parts = self.decoder_parts(output_features_body)
 
         return output_body, output_up_low, output_parts
@@ -379,7 +271,7 @@ class PascalPartModel(pl.LightningModule):
         loss_up_low = self.cross_entropy_loss(out_up_low, truth_up_low_mask.squeeze(1))
         loss_parts = self.cross_entropy_loss(out_parts, masks.squeeze(1))
 
-        total_loss = 0.4*loss_body + 0.2*loss_up_low + 0.4*loss_parts
+        total_loss = 0.34*loss_body + 0.25*loss_up_low + 0.4*loss_parts
         
         jaccard_loss_body = self.jaccard_loss(out_body, truth_body_mask.squeeze(1))
         jaccard_loss_up_low = self.jaccard_loss(out_up_low, truth_up_low_mask.squeeze(1))
@@ -405,17 +297,15 @@ class PascalPartModel(pl.LightningModule):
         self.log('train_jaccard_index_mIoU_2', jaccard_index_parts, prog_bar=True, on_step=True, on_epoch=True)
 
         # return different losses according to freeze pipeline
-        if self.current_epoch <= 1:
-            return loss_body + loss_up_low
+        if self.current_epoch <= 2:
+            return loss_body 
+        elif self.current_epoch <= 5:
+            return loss_up_low
 
-        elif self.current_epoch <= 3:
+        elif self.current_epoch <= 10:
             return loss_parts
-        
-        elif self.current_epoch <= 6:
-            return total_loss
-        
         else:
-            return loss_parts
+            return total_loss
 
     def validation_step(self, batch, batch_idx):
         """
@@ -462,17 +352,15 @@ class PascalPartModel(pl.LightningModule):
 
 
         # return different losses according to freeze pipeline
-        if self.current_epoch <= 1:
-            return loss_body + loss_up_low
+        if self.current_epoch <= 2:
+            return loss_body 
+        elif self.current_epoch <= 5:
+            return loss_up_low
 
-        elif self.current_epoch <= 3:
+        elif self.current_epoch <= 10:
             return loss_parts
-        
-        elif self.current_epoch <= 6:
-            return total_loss
-        
         else:
-            return loss_parts
+            return total_loss
 
 
     def configure_optimizers(self):
@@ -485,14 +373,9 @@ class PascalPartModel(pl.LightningModule):
             lr=self.learning_rate,
         )
 
-        # scheduler = lr_scheduler.StepLR(
-        #     optimizer,
-        #     step_size=5,
-        #     gamma=0.3,
-        # )
         scheduler = lr_scheduler.CosineAnnealingLR(
             optimizer,
-            T_max=50,
+            T_max=100,
         )
 
         return [optimizer], [scheduler]
--- train.py
+++ train.py
@@ -3,113 +3,111 @@ import pandas as pd
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from torch.utils.data import DataLoader
-import pytorch_lightning as pl
-from torchvision import transforms
 from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
-
-from dataset import PascalPartDataset
-# from lightning_module import PascalPartModel
-from pose_estimation_sam_module import PascalPartModel
-# from old_pose_estimation_sam_modeule import PascalPartModel
-
 import mlflow
 import time
 
-# set random seeds
+from dataset import PascalPartDataset
+from pose_estimation_sam_module import PascalPartModel
+
+# Set random seeds for reproducibility
 seed = 142
 pl.seed_everything(seed=seed, workers=True)
 
-
-
 def train():
+    """
+    Prepares data loaders, initializes the model, and trains the model using PyTorch Lightning.
+
+    This function handles the following steps:
+    - Reads training and validation image paths.
+    - Sets up data augmentation and normalization.
+    - Initializes datasets and data loaders.
+    - Configures the model, trainer, and callbacks.
+    - Integrates MLflow for experiment tracking.
+    - Starts the training process.
+    """
+
+    # Initialize empty DataFrames to store image and mask paths
     df_train_paths = pd.DataFrame({'PATH_TO_IMAGE': [], 'PATH_TO_MASK': []})
     df_val_paths = pd.DataFrame({'PATH_TO_IMAGE': [], 'PATH_TO_MASK': []})
 
-    # extract names of images and masks for train and val splts
+    # Define the train and validation splits
     splits = ["train_id", "val_id"]
     splits_samples = {}
 
+    # Read image IDs from split files
     for split in splits:
         with open(f'/storage/AIDA_PROJECTS/egor.koptelov/MIL_test_task/hierarchical_segmentation/data/{split}.txt') as f:
             splits_samples[split] = f.read().splitlines()
 
+    # Define the base path to the dataset
     path_to_raw = '/storage/AIDA_PROJECTS/egor.koptelov/MIL_test_task/Pascal-part'
 
-    # create dataframes with full paths to images and masks
+    # Create DataFrames with full paths to images and masks for training and validation sets
     df_train_paths['PATH_TO_IMAGE'] = [f'{path_to_raw}/Pascal-part/JPEGImages/{id_img}.jpg' for id_img in splits_samples['train_id']]
     df_train_paths['PATH_TO_MASK'] = [f'{path_to_raw}/Pascal-part/gt_masks/{id_mask}.npy' for id_mask in splits_samples['train_id']]
     df_val_paths['PATH_TO_IMAGE'] = [f'{path_to_raw}/Pascal-part/JPEGImages/{id_img}.jpg' for id_img in splits_samples['val_id']]
     df_val_paths['PATH_TO_MASK'] = [f'{path_to_raw}/Pascal-part/gt_masks/{id_mask}.npy' for id_mask in splits_samples['val_id']]
 
-    # # upload augmented samples 
+    # Upload augmented samples 
     # df_augment_paths = pd.DataFrame({'PATH_TO_IMAGE': [], 'PATH_TO_MASK': []})
     # df_augment_paths['PATH_TO_IMAGE'] = [f'{path_to_raw}/augmented_train_samples/JPEGImages/{id_img}.jpg' for id_img in splits_samples['train_id']]
     # df_augment_paths['PATH_TO_MASK'] = [f'{path_to_raw}/augmented_train_samples/gt_masks/{id_mask}.npy' for id_mask in splits_samples['train_id']]
 
-    # # add augmented paths to dataset
-
+    # Add augmented paths to dataset
     # df_train_paths = pd.concat([df_train_paths, df_augment_paths])
 
-
-    # specify transforms 
-    # transform = transforms.Compose([
-    #     transforms.Resize((256, 256)),
-    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    # ])
-
+    # Data augmentation and normalization transformations
     transform = A.Compose([
         A.Resize(256, 256),
-        A.Normalize(
-            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225],
-            # max_pixel_value=255.0,
-        ),
+        A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ToTensorV2()
     ])
 
-    # create datasets
+    # Create datasets for training and validation
     train_dataset = PascalPartDataset(df_train_paths, transform=transform)
     val_dataset = PascalPartDataset(df_val_paths, transform=transform)
 
-    batch_size = 12
-    # create dataloaders
+    # Define the batch size for training
+    batch_size = 16
+
+    # Create DataLoaders for training and validation
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=63)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=63)
 
-    # create a model
+    # Initialize the model with specific parameters
     pl_model = PascalPartModel(
         encoder_name='resnet34',
-        encoder_weights='imagenet', 
-        learning_rate=1e-3, 
-        transform=transform, 
-        # num_classes=7
+        encoder_weights='imagenet',
+        learning_rate=1e-3,
+        transform=transform
     )
     
+    # Define callbacks for early stopping and learning rate monitoring
     callbacks = [
         EarlyStopping(monitor='val_loss', patience=3, min_delta=1e-3),
         LearningRateMonitor(logging_interval='epoch'),
     ]
 
+    # Configure the PyTorch Lightning trainer
     trainer = pl.Trainer(
-        max_epochs=23,
+        max_epochs=18,
         accelerator='gpu',
         devices=1    
     )
 
+    # command to starting mlflow server
     #  mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./lightning_logs --host 0.0.0.0 --port 3033
-    
-    # specify server port
-    port = '3033'
 
-    # speify model name
+    # Configure MLflow for logging experiments
+    port = '3033'
     timestamp = time.strftime('%Y%m%d-%H%M%S')
     model_name = f'DeepLabV3_{timestamp}'
 
-    # Set tracking uri for mlflow server
     mlflow.set_tracking_uri(f'http://localhost:{port}')
-    # specify mlflow logging frequency
     log_freq = 10
 
-    # Auto log all MLflow entities
+    # Enable MLflow automatic logging
     mlflow.pytorch.autolog(
         log_every_n_step=log_freq,
         log_models=True,
@@ -121,19 +119,17 @@ def train():
         checkpoint_save_freq='epoch',
     )
 
+    # Set the experiment name in MLflow
     project = 'PascalPart'
-    # set up experiment name
     mlflow.set_experiment(project)
     experiment = mlflow.get_experiment_by_name(project)
 
-    # start mlflow logging
+    # Start MLflow run and train the model
     with mlflow.start_run(
         experiment_id=experiment.experiment_id,
         run_name=model_name,
     ):
-        # strart training the model
         trainer.fit(pl_model, train_dataloader, val_dataloader)
-
 
 if __name__ == '__main__':
     train()
